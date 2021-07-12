439e723218695e9b10de793af6fc4457da53ae56
Venlin0067 <68690458+Venlin0067@users.noreply.github.com>
2021-07-12 11:50:43 +0800

    Initial commit

---
+++ app/src/main/java/com/venlin/fragment/Question.kt
@@ -0,0 +1,8 @@
+package com.venlin.fragment
+
+data class Question(
+
+    val text: String,
+    val answers: List<String>
+
+)
---
+++ app/src/main/java/com/venlin/fragment/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.venlin.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.venlin.fragment.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding : FragmentWelcomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        binding.btnLogin.setOnClickListener()
+        {
+            Navigation.findNavController(it).navigate(R.id.action_welcomeFragment_to_gameFragment)
+        }
+
+        return binding.root
+    }
+}
